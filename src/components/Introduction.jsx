dece4373645c05dbca99762a789b607dbca2c5fd
Pabitra <pkg2000696@gmail.com>
2025-10-07 14:14:50 +0530

    Profile image issue

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Introduction = () => {
   return (
-    <section class=" md:mt-30 sm:mt-15 mb-15 lg:mb-30 lg:mr-20 lg:ml-18">
-      <div class="flex justify-center items-center gap-6 max-lg:block" >
-        <div id="image" class="bg-bluish h-60 w-60 rounded-full flex items-center justify-center sm:ml-15 max-sm:ml-10" >
-          <img src="/public/Profile_Image.jpeg" alt="Profile Image Pabitra" class="rounded-full h-54 w-54" />
+    <section className=" md:mt-30 sm:mt-15 mb-15 lg:mb-30 lg:mr-20 lg:ml-18">
+      <div className="flex justify-center items-center gap-6 max-lg:block" >
+        <div id="image" className="bg-bluish h-60 w-60 rounded-full flex items-center justify-center sm:ml-15 max-sm:ml-10" >
+          <img src="/Profile_Image.jpeg" alt="Profile Image Pabitra" className="rounded-full h-54 w-54" />
         </div>
-        <div id="text" class="max-lg:m-10" >
-          <h1 class="text-4xl">Hello this is Pabitra here.</h1>
-          <p class="text-gray-400 " >A front-end developer who builds accessible, responsive web experiences. I also have experience in AI/ML model training and hosting.</p>
+        <div id="text" className="max-lg:m-10" >
+          <h1 className="text-4xl">Hello this is Pabitra here.</h1>
+          <p className="text-gray-400 " >A front-end developer who builds accessible, responsive web experiences. I also have experience in AI/ML model training and hosting.</p>
           <a
             href="#project"
             rel='noopener noreferrer'
